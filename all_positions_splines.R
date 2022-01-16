b99e0f12e3c37f2203dfa9f93e8bb415d169062f
shughes1000 <shughes1000@verizon.net>
2022-01-15 20:06:16 -0500

    photos

--- all_positions_splines.R
+++ all_positions_splines.R
@@ -4,7 +4,7 @@ library(tidyverse)
 ADP_average = c(1:200)
 df = data.frame(ADP_average, qb_predict, rb_predict, wr_predict, te_predict, qb_deriv, rb_deriv, wr_deriv, te_deriv)
 
-# This part is unnecessary because predict has a built in derivitave parameter
+# This part is unnecessary because predict has a built in derivative parameter
 # df = df %>% mutate(qb_deriv = qb_predict - lag(qb_predict, default = first(qb_predict)))
 # df = df %>% mutate(rb_deriv = rb_predict - lag(rb_predict, default = first(rb_predict)))
 # df = df %>% mutate(wr_deriv = wr_predict - lag(wr_predict, default = first(wr_predict)))
@@ -14,16 +14,24 @@ df = data.frame(ADP_average, qb_predict, rb_predict, wr_predict, te_predict, qb_
 
 ggplot(data = df,
        mapping = aes(x = ADP_average))+
-  geom_line(aes(y = qb_predict, col = "qb_predict"), size = 3)+
-  geom_line(aes(y = rb_predict, col = "rb_predict"), size = 3)+
-  geom_line(aes(y = wr_predict, col = "wr_predict"), size = 3)+
-  geom_line(aes(y = te_predict, col = "te_predict"), size = 3)
+  geom_line(aes(y = qb_predict, col = "QB"), size = 3)+
+  geom_line(aes(y = rb_predict, col = "RB"), size = 3)+
+  geom_line(aes(y = wr_predict, col = "WR"), size = 3)+
+  geom_line(aes(y = te_predict, col = "TE"), size = 3)+
+  labs(title = "Expected Points Per Game by Average Draft Position",
+      y = "Expected Points Per Game",
+      x = "Average Draft Position",
+      col = "Position")
 
 ggplot(data = df,
        mapping = aes(x = ADP_average))+
-  geom_line(aes(y = qb_deriv, col = "qb_deriv"), size = 3)+
-  geom_line(aes(y = rb_deriv, col = "rb_deriv"), size = 3)+
-  geom_line(aes(y = wr_deriv, col = "wr_deriv"), size = 3)+
-  geom_line(aes(y = te_deriv, col = "te_deriv"), size = 3)
+  geom_line(aes(y = qb_deriv, col = "QB"), size = 3)+
+  geom_line(aes(y = rb_deriv, col = "RB"), size = 3)+
+  geom_line(aes(y = wr_deriv, col = "WR"), size = 3)+
+  geom_line(aes(y = te_deriv, col = "TE"), size = 3)+
+  labs(title = "Derivative of Expected Points Per Game by Average Draft Position",
+       y = "Expected Points Per Game Derivative",
+       x = "Average Draft Position",
+       col = "Position")
 
 write.csv(df, "spline_predictions.csv", row.names = FALSE)
